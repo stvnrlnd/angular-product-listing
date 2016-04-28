e1c9623e9b10ef66d1ffec56e98c1bca5687a008
Steven Roland <steven@mojave.io>
2016-04-27 20:58:57 -0400

    Playing with controllers and object
    - Created a item object.
    - Created a controller to handle the object.
    - Tested that the object property values are visible on the index page.

--- app/core.js
+++ app/core.js
@@ -1,3 +1,12 @@
 (function(){
   'use strict';
+  angular.module('store', [])
+    .controller('storeController', function() {
+      this.product = item;
+    });
+    var item = {
+      name: 'Fries',
+      price: 2.95,
+      description: 'Some yummy fried potato sticks.'
+    };
 })();
