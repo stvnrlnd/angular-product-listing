f753e8a35c1f4e20e599cd88ce90ccc722268bd6
Steven Roland <steven@mojave.io>
2016-04-27 21:53:05 -0400

    Playing with directives
    - Used ngRepeat to print out all items in the items array.
    - Used ngHide to hide items that were sold out.
    - Used ngShow to hide the 'Add to Cart' button when the item is
    unavailable for purchase.

--- app/core.js
+++ app/core.js
@@ -2,11 +2,22 @@
   'use strict';
   angular.module('store', [])
     .controller('storeController', function() {
-      this.product = item;
+      this.products = items;
     });
-    var item = {
-      name: 'Fries',
-      price: 2.95,
-      description: 'Some yummy fried potato sticks.'
-    };
+    var items = [
+      {
+        name: 'Fries',
+        price: 2.95,
+        description: 'Some yummy fried potato sticks.',
+        canPurchase: true,
+        soldOut: false
+      },
+      {
+        name: 'Black Bean Burger',
+        price: 4.95,
+        description: 'Yummy black bean burger.',
+        canPurchase: true,
+        soldOut: false
+      }
+    ];
 })();
